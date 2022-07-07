8af36f67438f17f6e71032a4709bc44179748a5e
MartinKraychev <83907783+MartinKraychev@users.noreply.github.com>
2022-07-07 20:03:20 +0100

    end test

--- Movies/settings.py
+++ Movies/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.getenv('SECRET_KEY', '')
 # SECURITY WARNING: don't run with debug turned on in production!
 
-DEBUG = os.getenv('DEBUG', 'True') == 'True'
+DEBUG = os.getenv('DEBUG', 'False') == 'True'
 
 APP_ENVIRONMENT = os.getenv('APP_ENVIRONMENT', '')
 
